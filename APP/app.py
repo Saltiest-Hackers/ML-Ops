463da12f42351050eeb2d11c41a64652bcc35477
mariayasar <m.dubrovskaya@gmail.com>
2020-03-03 08:49:30 -0800

    cleaned up app.py

--- APP/app.py
+++ APP/app.py
@@ -3,24 +3,23 @@ import psycopg2
 from flask_sqlalchemy import SQLAlchemy 
 from decouple import config
 
+
+
 def create_app():
     
     app=Flask(__name__)
+     # connect to the PostgreSQL server
+    conn = psycopg2.connect("postgres://" + config("POSTGRES_USERNAME") + ":" + config("POSTGRES_PASSWORD") + "@raja.db.elephantsql.com:5432/mozfsrjp")
 
-
+        
     @app.route('/')
     def hello():
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect("postgres://" + config("POSTGRES_USERNAME") + ":" + config("POSTGRES_PASSWORD") + "@raja.db.elephantsql.com:5432/mozfsrjp")
-        message="Hello, world"
+        """Returns 100 most salty comments"""
         cur=conn.cursor()
         cur.execute("(SELECT ROW_TO_JSON(c) FROM (SELECT * FROM comments ORDER BY saltiness DESC LIMIT 100) c)")
-        # cur.execute("SELECT * FROM comments ORDER BY saltiness DESC LIMIT 100")
         result_set=cur.fetchall()
         final=[]
-
         for i in range(len(result_set)):
-            #print(result_set[i][0])
             final.append(result_set[i][0])
         
         return json.dumps(final) 
@@ -36,32 +35,4 @@ def create_app():
     #     cur.fetchall()
     #     return message
 
-
-   
-
-
     return app
-
-
-
-#Trying to use SQLAlchemy: 
-
-    # DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=config("POSTGRES_USERNAME"),pw=config("POSTGRES_PASSWORD"),url=config("POSTGRES_URL"),db=config("POSTGRES_DB"))
-
-    # app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
-
-    # DB = SQLAlchemy(app)    
-
-    # DB.init_app(app)
-
-    # class Comment(DB.Model):
-    #     id = DB.Column(DB.Integer, primary_key=True)
-    #     author = DB.Column(DB.String)
-    #     comment_text = DB.Column(DB.Unicode)
-
-
-    # @app.route('/')
-    # def hello():
-    #     comments = Comment.query.limit(5).all()
-    #     return render_template("home.html", comments=comments)
